e75486197899dcf5cb46165fbcb3f1d989d22f87
DylanMashini <dylanmashini123@gmail.com>
2023-12-26 19:19:51 -0500

    Reduced Margin Size and Moved frontmatter parameters to deticated file

---
+++ 00 - Frontmatter.md
@@ -0,0 +1,7 @@
+---
+geometry: margin=2cm
+header-includes: |
+    \usepackage{pdfpages}
+toc: true
+include-before: \includepdf{./CoverPage.pdf}
+---
--- CreatePDF.sh
+++ CreatePDF.sh
@@ -5,15 +5,6 @@ echo "Updating Output.pdf"
 # Temporary file to hold the concatenation
 temp_file=$(mktemp)
 
-# echo -e "---\noutput:\n pdf_document:\n	includes:\n	    before_body: CoverPage.tex\n---\n\`{=latex}\includepdf{CoverPage.pdf}\`" >> "$temp_file"
-echo -e "---" >> "$temp_file"
-echo -e "header-includes: |" >> "$temp_file"
-echo -e "    \usepackage{pdfpages}" >> "$temp_file"
-echo -e "toc: true\n" >> "$temp_file"
-echo -e "include-before: \includepdf{./CoverPage.pdf}" >> "$temp_file"
-echo -e "---\n" >> "$temp_file"
-
-
 # Iterate through all markdown files in the current directory
 for file in *.md; do
     # Ensure that each file starts on a new page by appending a page break
